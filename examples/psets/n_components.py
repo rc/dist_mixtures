1dfcf023a7bdb74f9dc020b2714cefd8ccb68144
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-02-15 12:24:10 +0100

    update examples/psets/n_components.py to use binned fitting

--- examples/psets/n_components.py
+++ examples/psets/n_components.py
@@ -1,25 +1,25 @@
 """
-Fitting parameter sets configuration.
+Parameter sets with a changing number of von Mises mixture components.
 
 By default, unspecified options are taken from a previous set. In case of the
 first set, defaults are provided in the code. Command-line options can be used
 to override the settings below.
 """
+import dist_mixtures.mixture_von_mises as mvm
 
 parameter_sets = [
     {
+        'model_class' : mvm.VonMisesMixtureBinned,
         'n_components' : 1,
         'parameters' : [2.0, 0.0], # Starting values.
         'solver' : ('bfgs', {'gtol' : 1e-8}),
-        'output_dir' : 'output/1',
+        'output_dir' : 'output/n_components',
     },
     {
         'n_components' : 2,
         'parameters' : 'previous', # Starting value from the previous run.
-        'output_dir' : 'output/2',
     },
     {
         'n_components' : 3,
-        'output_dir' : 'output/3',
     },
 ]
