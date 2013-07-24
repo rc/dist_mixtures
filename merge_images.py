82706ba3738d5d68af313719477e6af240dfc80b
Robert Cimrman <cimrman3@ntc.zcu.cz>
2013-07-24 17:27:03 +0200

    new merge_best()

--- merge_images.py
+++ merge_images.py
@@ -42,6 +42,41 @@ def merge_selected():
         print merged_name
         new_img.save(merged_name)
 
+def merge_best():
+    best = {
+        '249_09' : [3, 3],
+        '180_09' : [3, 3],
+        '216_09' : [3, 3],
+        '228_09' : [3, 3],
+        '261_09' : [3, 3],
+        '414_09' : [3, 3],
+        '291_09' : [3, 3],
+        '177_09' : [3, 3],
+        '183_09' : [3, 3],
+    }
+
+    args = sys.argv[1:]
+    root_dir = args[0]
+    print root_dir
+
+    for dirname in os.listdir(os.path.abspath(root_dir)):
+        dd = os.path.join(root_dir, dirname)
+        if not os.path.isdir(dd): continue
+        print dd
+
+        aux = glob(dd + '/*-data*')
+        filenames = sorted(glob(dd + '/*-fit*'))
+        dataset = os.path.basename(filenames[0]).split('-')[0]
+        filenames = [ii for ii in filenames
+                     for ic in best[dataset]
+                     if int(ii.split('-')[-1][0]) == (ic - 1)]
+        # Both are the same - no need to merge.
+        new_img = merge_images(aux + filenames[:1], 1, 2, (800, 600))
+
+        merged_name = os.path.join(root_dir, dirname + '-best.png')
+        print merged_name
+        new_img.save(merged_name)
+
 def merge_ellipse_imagej():
     args = sys.argv[1:]
 
@@ -68,4 +103,5 @@ def merge_ellipse_imagej():
             new_img.save(merged_name)
 
 # merge_selected()
+# merge_best()
 # merge_ellipse_imagej()
